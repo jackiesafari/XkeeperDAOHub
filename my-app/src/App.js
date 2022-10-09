96d5eda4dc7f91a104ae420c8ba06ed20931025b
jackiesafari <107513244+jackiesafari@users.noreply.github.com>
2022-10-09 00:47:42 -0500

    update scientific articles

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -18,7 +18,33 @@ function App() {
           Submit research paper
         </a>
         <p>Apply to be a Verifier</p>
-      <p> Approved submissions </p>
+        <a
+          className="App-link"
+          href="https://gateway-proxy-bee-2-0.gateway.ethswarm.org/bzz/e9c20ce41ebe48a7cbb4fac63f5aea87def4390adcbd847341c04fdb8e828da8/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Approved submission #1 (Pygmy rabbit)
+        </a>
+
+        <a
+          className="App-link"
+          href="https://gateway-proxy-bee-1-0.gateway.ethswarm.org/bzz/5e9319411a4cd4e4e1ac88ccb640c1410360a9c319234151161eac20b9fd0cff/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Approved submission #2 (Desert Tortoise)
+        </a>
+
+        <a
+          className="App-link"
+          href="https://gateway-proxy-bee-3-0.gateway.ethswarm.org/bzz/135a29857fb602d215be9d40c69794eaa81f38b07160a8eb1cc02bfa06b9d2f6/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Approved submission #3 (The Pika)
+        </a>
+        <p>Enter the Metaverse and dive into science</p>
       </header>
       
     </div>
